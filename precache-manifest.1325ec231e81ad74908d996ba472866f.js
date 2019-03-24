c1fde9a7f84dd7913ed4f380e601dc0bd62eb530
XuanD <xuandeng1029@gmail.com>
2019-03-24 21:58:52 +0800

    Updates

---
+++ precache-manifest.1325ec231e81ad74908d996ba472866f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  },
+  {
+    "revision": "5202afb3dfc498cbd4d3",
+    "url": "/todo/static/js/main.af2caba2.chunk.js"
+  },
+  {
+    "revision": "37633691b9724b784dbc",
+    "url": "/todo/static/js/2.ff439d2d.chunk.js"
+  },
+  {
+    "revision": "5202afb3dfc498cbd4d3",
+    "url": "/todo/static/css/main.6afc5cb4.chunk.css"
+  },
+  {
+    "revision": "2271bec57b7cac2b0f3aa9749062ec57",
+    "url": "/todo/index.html"
+  }
+];
